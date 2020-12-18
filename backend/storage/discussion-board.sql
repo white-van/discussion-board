6a3fe2f95db1360e32bad2262f35a107119daee3
Nive <nivedithakani@gmail.com>
2020-12-17 20:53:49 -0500

    Fix name changes

--- backend/storage/discussion-board.sql
+++ backend/storage/discussion-board.sql
@@ -15,14 +15,14 @@ CREATE TABLE institutions (
 );
 
 CREATE TABLE semesters (
-  semester_idSERIAL PRIMARY KEY,
+  semester_id SERIAL PRIMARY KEY,
   semester_name varchar(100) NOT NULL,
-  institution_id int NOT NULL REFERENCES institutions (id) ON DELETE CASCADE
+  institution_id int NOT NULL REFERENCES institutions (institution_id) ON DELETE CASCADE
 );
 
 CREATE TABLE administrators (
   user_id int REFERENCES users (user_id) ON DELETE CASCADE,
-  institution_id int REFERENCES institutions (id) ON DELETE CASCADE,
+  institution_id int REFERENCES institutions (institution_id) ON DELETE CASCADE,
   PRIMARY KEY (user_id, institution_id)
 );
 
@@ -30,13 +30,13 @@ CREATE TABLE courses (
   course_id SERIAL PRIMARY KEY,
   course_name varchar(100),
   course_code varchar(50) NOT NULL,
-  semester_id int REFERENCES semesters (id) ON DELETE CASCADE,
-  is_archived boolean DEFAULT 0
+  semester_id int REFERENCES semesters (semester_id) ON DELETE CASCADE,
+  is_archived boolean DEFAULT false
 );
 
 CREATE TABLE enrolment (
   user_id int REFERENCES users (user_id) ON DELETE CASCADE,
-  course_id int REFERENCES courses (id) ON DELETE CASCADE,
+  course_id int REFERENCES courses (course_id) ON DELETE CASCADE,
   position position_types NOT NULL,
   question_contributions int DEFAULT 0 NOT NULL,
   answer_contributions int DEFAULT 0 NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE enrolment (
 CREATE TABLE categories (
   category_id SERIAL PRIMARY KEY,
   category_name varchar(100) NOT NULL,
-  course_id int REFERENCES courses (course_id ) ON DELETE CASCADE
+  course_id int REFERENCES courses (course_id) ON DELETE CASCADE
 );
 
 CREATE TABLE thread (
@@ -62,25 +62,25 @@ CREATE TABLE thread (
   view_count int DEFAULT 0 NOT NULL,
   created_at timestamp with time zone DEFAULT (now()),
   updated_at timestamp with time zone DEFAULT (now()),
-  is_locked boolean DEFAULT 0,
-  is_anon boolean DEFAULT 0
+  is_locked boolean DEFAULT false,
+  is_anon boolean DEFAULT false
 );
 
 CREATE TABLE thread_categories (
-  category_id int REFERENCES categories (id) ON DELETE CASCADE,
-  thread_id int REFERENCES thread (id) ON DELETE CASCADE,
+  category_id int REFERENCES categories (category_id) ON DELETE CASCADE,
+  thread_id int REFERENCES thread (thread_id) ON DELETE CASCADE,
   PRIMARY KEY (category_id, thread_id)
 );
 
 CREATE TABLE award_types (
   award_type_id SERIAL PRIMARY KEY,
-  name varchar(50) NOT NULL,
-  image int
+  award_name varchar(50) NOT NULL,
+  award_image int
 );
 
 CREATE TABLE awards (
-  id SERIAL PRIMARY KEY,
+  award_id SERIAL PRIMARY KEY,
   award_type int REFERENCES award_types (award_type_id) ON DELETE CASCADE,
-  thread int REFERENCES thread (id) ON DELETE CASCADE,
+  thread int REFERENCES thread (thread_id) ON DELETE CASCADE,
   gifter int REFERENCES users (user_id) ON DELETE CASCADE
 );
