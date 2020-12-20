ef8fb1a1a92ceee05759d303a402b6cf9cb457e1
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2020-12-20 17:57:46 -0500

    clean up storybook

---
+++ frontend/components/Button/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./button.css";
+import { Button as ChakraButton } from '@chakra-ui/react';
+
+export interface ButtonProps {
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button: React.FC<ButtonProps> = ({
+  label,
+  ...props
+}) => {
+  return (
+    <ChakraButton
+    colorScheme="blue"
+    {...props}
+    >
+      {label}
+    </ChakraButton>
+  );
+};
