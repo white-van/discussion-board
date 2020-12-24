41cb71f865d698131311829f2e191ba9060044f3
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-12-24 01:20:42 -0500

    adds linting

--- backend/httpd/main.go
+++ backend/httpd/main.go
@@ -9,5 +9,8 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err := r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if err != nil {
+		panic("Failed to start server")
+	}
 }
