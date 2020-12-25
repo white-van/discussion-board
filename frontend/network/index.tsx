672c658a65de40ab737c8375839b84a6e64920dd
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2020-12-24 23:57:23 -0500

    update typing for network

--- frontend/network/index.tsx
+++ frontend/network/index.tsx
@@ -6,28 +6,30 @@ const enforceTrailingSlash = (url: string): string => {
   return url.endsWith("/") ? url : url + "/";
 };
 
-export function getRequest<T>(
+export function getRequest<ResponseType>(
   uri: string,
   searchParams: string
-): Promise<AxiosResponse<T>> {
+): Promise<AxiosResponse<ResponseType>> {
   const params = searchParams ? searchParams : "";
   return axios.get(enforceTrailingSlash(`${serverUrl}${uri}`) + params);
 }
 
-export function deleteRequest<T>(uri: string): Promise<AxiosResponse<T>> {
+export function deleteRequest<ResponseType>(
+  uri: string
+): Promise<AxiosResponse<ResponseType>> {
   return axios.delete(enforceTrailingSlash(`${serverUrl}${uri}`));
 }
 
-export function patchRequest<T>(
+export function patchRequest<DataType, ResponseType>(
   uri: string,
-  data: T
-): Promise<AxiosResponse<T>> {
+  data: DataType
+): Promise<AxiosResponse<ResponseType>> {
   return axios.patch(enforceTrailingSlash(`${serverUrl}${uri}`), data);
 }
 
-export function postRequest<T>(
+export function postRequest<DataType, ResponseType>(
   uri: string,
-  data: T
-): Promise<AxiosResponse<T>> {
+  data: DataType
+): Promise<AxiosResponse<ResponseType>> {
   return axios.post(enforceTrailingSlash(`${serverUrl}${uri}`), data);
 }
