a8512df88d6aae985ff0b7f24bedc50e7aee25a6
Jarrod Servilla <jarrod.servilla@mail.utoronto.ca>
2020-12-27 22:38:14 -0500

    remove selectors

--- frontend/tests/stores/mockSlice.test.tsx
+++ frontend/tests/stores/mockSlice.test.tsx
@@ -1,25 +1,9 @@
 import { setMagicNumber } from "../../stores/mockSlice/actions";
-import { mockReducerName } from "../../stores/mockSlice/adapter";
 import reducer, { initialState } from "../../stores/mockSlice/reducer";
-import { magicNumberSelector } from "../../stores/mockSlice/selectors";
 
 describe("Reducers", () => {
   test("setMagicNumber", () => {
     const state = reducer(initialState, setMagicNumber(50));
     expect(state.magicNumber).toEqual(50);
-  });
-});
-
-const mockSelectorState = {
-  [mockReducerName]: {
-    ...initialState,
-  },
-};
-
-describe("Selectors", () => {
-  test("magicNumberSelector", () => {
-    expect(magicNumberSelector(mockSelectorState)).toEqual(
-      initialState.magicNumber
-    );
   });
 });
