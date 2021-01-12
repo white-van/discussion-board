3586bcc9fa2226d6cbc59ace2e5160174f2b43ba
Nive <nivedithakani@gmail.com>
2021-01-12 01:55:43 -0500

    Add FeaturesList component

--- frontend/components/FeatureCard/FeatureCard.tsx
+++ frontend/components/FeatureCard/FeatureCard.tsx
@@ -1,7 +1,5 @@
-import "./featureCard.module.css";
-
-import { Box as ChakraBox } from "@chakra-ui/react";
-import { Center, ChakraProvider, Icon, Text } from "@chakra-ui/react";
+import { Box as ChakraBox, useColorModeValue } from "@chakra-ui/react";
+import { Center, Icon, Text } from "@chakra-ui/react";
 import React from "react";
 import { IconType } from "react-icons";
 
@@ -28,22 +26,22 @@ export const FeatureCard: React.FC<FeatureCardProps> = ({
       width="270px"
       height="200px"
       position="relative"
-      bg="white"
+      bg={useColorModeValue("white.light", "white.dark")}
       rounded="3xl"
       pt="5"
       pl="5"
       pr="5"
     >
       <Center>
-        <Icon as={icon} w={50} h={50} color="brand.100" />
+        <Icon as={icon} w={50} h={50} color="primary.light" />
       </Center>
       <Center>
-        <Text fontSize="3xl" fontWeight="bold" color="brand.300">
+        <Text fontSize="3xl" fontWeight="bold" color="grey.charcoal">
           {title}
         </Text>
       </Center>
       <Center>
-        <Text fontSize="md" align="center" color="brand.300">
+        <Text fontSize="md" align="center" color="grey.charcoal">
           {description}
         </Text>
       </Center>
